e379e384c605eb8fb4ebf2d5ee4c0c410f8005e4
Aluno <aluno@fiemguba.srv>
2026-06-02 19:54:05 -0300

    Fix config de bancos

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,16 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '',
     database : process.env.DB_DATABASE || 'manutencao_maquinas',
-    port: 3306
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: {
+      rejectUnauthorized: false
+    }
 });
 
 pool.getConnection((err, connection) => {
